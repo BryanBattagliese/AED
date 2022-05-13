78196b2fddaed53b291eba52b30dcd71c8929761
BryanBattagliese <103158012+BryanBattagliese@users.noreply.github.com>
2022-05-12 23:42:20 -0300

    Update EjemploTipos.cpp

--- 01-EjemploTiposdeDatos/EjemploTipos.cpp
+++ 01-EjemploTiposdeDatos/EjemploTipos.cpp
@@ -1,4 +1,4 @@
-#include <cassert>
+#include <cassert>  
 #include <string>
 #include <iomanip>
 
@@ -6,58 +6,65 @@ using namespace std::literals;
 
 int main ()
 {
-	// PRUEBA DE DATO TIPO: BOOL
+    // PRUEBA DE DATO TIPO: BOOL (values true or false)
 	
     assert (true);
-    assert (false==false);
-    assert (true!=false);
+    assert (false == false);
+    assert (true != false);
     assert (not false);
-    assert (not false==true);
+    assert (not false == true);
     assert (false or true);
     assert (true and true);
-    assert (false or true and false==false);
+    assert (false or true and false == false);
     assert (true or true and false);
     
 
-    // PRUEBA DE DATO TIPO: CHAR
+    // PRUEBA DE DATO TIPO: CHAR (utiliza la tabla ASCII. (ej: 'A' == 65)). Se utilizan las comiilas simples 'x'.
     
-    assert (82 == 'R');
-    assert (114 == 'r');
-    assert ('9' == 57);
-    assert ('k' == 107);
+    assert ('a' != 'A');
+    assert ('a' > 'A');
+    assert ('A' == 65);
+    assert ('a' - 32 == 'A');
+    assert ('5' != '4' + '1');
+    assert (1 == 'B' - 'A');
+        
+
+    // PRUEBA DE DATO TIPO: UNSIGNED (conjunto de numeros naturales N). Se utiliza la 'u', ya que sin esta, serian "int's".
+
+    assert (2u + 4u < 10u);
+    assert (3u - 2u != 6u);
+    assert (2u * 2u == 2u + 2u);
+    assert (25u / 5u > 4u);
+    assert (1u ==  11u % 2u);
     
 
-    // PRUEBA DE DATO TIPO: UNSIGNED
+    // PRUEBA DE DATO TIPO: INT (contjunto de numeros enteros Z).
 
-    assert (2+4 < 10);
-    assert (2+2 == 2*2);
-    assert (1 == 3-2);
-    assert (5*3 == 15);
-    
-
-    // PRUEBA DE DATO TIPO: INT
-
+    assert (6+6 <= 12);
     assert (-19 == -10-9);
-    assert (48 == 50-2);
-    assert (22 == 10*2 + 2);
-    assert (-2  == 4/-2 );
+    assert (4 > -22);
+    assert (22 != 10*2);
+    assert (20 == 100/5);
+    assert (3 == 28%5);
 
 
-    // PRUEBA DE DATO TIPO: DOUBLE
+    // PRUEBA DE DATO TIPO: DOUBLE (conjunto de numeros reales R). 
+    // assert (0.0 == 0. and .0 == 0.0 and 0.0 == 0.0f and 0.0 == 0e1); ((f se refiere a "float")).
+
+    assert (2.0 != 1.0);
+    assert (0.0 - 1.0 == -1.0 );
+    assert (7.4 != 2.3 + 2.6);
+    assert (4.5 < 4.0 + 1.1);
+    assert (3.0 * 12.1 != 15.2);
+    assert (19.6 / 0.3 < 101.7);
+    assert (1.0 != 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
     
-    assert (2.0 == 1.0+1.0);
-    assert (0.1 == 1.0/10.0);
-    assert (4 == 4.0);
-    assert (2.0 == 1+1.0);
-    assert (3 == 2+1.0);
-    
 
-    // PRUEBA DE DATO TIPO: STRING
+    // PRUEBA DE DATO TIPO: STRING (tipo de dato compuesto, cadena de caracteres (char's)). Se utilizan las comillas "x".
 
     assert ("bryanAED"s == "bryan"s + "AED"s);
     assert ("bryanAED"s.length() == 8);
-    assert ("algoritmos"s == "algo"s + "ritmos"s);
-    assert ("algoritmos"s.length() > 0);
-    
-    
+    assert ("AA" < "BB");
+    assert ("AA" != "aa");
+
 }
