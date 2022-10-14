3ca7d9a305467f28873ae12148fa5dc7354a307f
BryanBattagliese <103158012+BryanBattagliese@users.noreply.github.com>
2022-10-14 19:07:34 -0300

    Add files via upload

--- TP15-TotalDeVentas/dim1.cpp
+++ TP15-TotalDeVentas/dim1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <array>
 
+/* Consigna 2: DIM 1
+   Dado los importes y meses (de 0 a 11), mostrar ventas totales
+   por mes.
+*/
+
+/* ANALISIS:
+   (VENTA x mes) * --> P --> VENTA´12 (venta'12 = array)
+   (VENTA =  N)
+
+   ej- (venta en $, mes)
+       (5,0) : (7,2) : (3,11) --->  $5 en enero; $7 en marzo; $3 en diciembre ...
+*/
+
+int main ()
+{
+   std::array<unsigned,12> total{0};
+   
+   for(unsigned venta, mes; std::cin >> venta >>mes;)
+      total.at(mes) += venta;
+
+   for(int i{0}; i<12 ;++i)
+      std::cout << total.at(i) << '\n'; 
+}
