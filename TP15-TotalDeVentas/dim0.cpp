9ab5e7930b6c9eb9607a7741e8cc8bedefc4e1d6
BryanBattagliese <103158012+BryanBattagliese@users.noreply.github.com>
2022-10-14 19:02:37 -0300

    Add files via upload

--- TP15-TotalDeVentas/dim0.cpp
+++ TP15-TotalDeVentas/dim0.cpp
@@ -1,1 +1,21 @@
+#include <iostream>  
 
+/* Consigna 1: DIM 0
+   Dado los importes, mostrar ventas totales.
+*/
+
+/* ANALISIS:
+
+   VENTA* --> P --> VENTA (entran multiplicidad de ventas, tambien puede entrar 0)
+   VENTA = N              (ventas = natural)
+
+*/
+
+int main ()
+{
+   unsigned total {0};                           
+   
+   for(unsigned venta; std::cin >> venta;)       
+      total += venta;                           
+      std::cout << total << '\n';                
+}
