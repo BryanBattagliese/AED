99ed54d8a1d1b7478dee00846115095024fbae41
BryanBattagliese <103158012+BryanBattagliese@users.noreply.github.com>
2022-10-16 13:10:37 -0300

    Add files via upload

--- TP15-TotalDeVentas/dim3.cpp
+++ TP15-TotalDeVentas/dim3.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <array>
+#include <string>
 
+/* Consigna 4: DIM 3
+   Dado los importes, meses, números de los tres vendedores, y
+   números de las cuatro regiones (0, 1, 2, 4), mostrar total de ventas por mes,
+   vendedor, y región.
+*/
+
+
+int main ()
+{
+    using std::array;
+    array<array<array<unsigned,12>,3>,4> total{0};
+
+    std::cout << "Ingrese importe de la venta, mes, vendedor y region correspondiente" << '\n';
+
+    for(unsigned venta, mes, vendedor, region; std::cin >> venta >> mes >> vendedor >> region;)
+        total[region][vendedor][mes] += venta;
+        
+    std::cout << "TOTAL DE VENTAS" << '\n';
+
+    for(int k{0}; k<4; ++k)
+    {   std::cout << '\n' << "REGION " << k << '\n';
+      
+      for(int i{0}; i<3; ++i)
+      {   std::cout << '\n' << "VENDEDOR " << i << '\n';    
+            
+            for(int j{0};j<12;++j)
+                std::cout <<  "MES " << j << " = " << total[k][i][j]<< '\n';      
+      }; 
+    }  
+}
