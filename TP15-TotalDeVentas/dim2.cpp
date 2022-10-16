7a98d4c1d009abc15bc543b0fa8d5e36f5800d48
BryanBattagliese <103158012+BryanBattagliese@users.noreply.github.com>
2022-10-15 23:33:10 -0300

    Add files via upload

--- TP15-TotalDeVentas/dim2.cpp
+++ TP15-TotalDeVentas/dim2.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <array>
 
+/* Consigna 3: DIM 2
+   Dado los importes, meses, y números de los tres vendedores
+   (0, 1, 2), mostrar total de ventas por mes y vendedor.
+*/
+
+int main ()
+{
+    using std::array;
+    array<array<unsigned,12>,3> total{0};
+
+    std::cout << "Ingrese importe de la venta, mes y vendedor correspondiente" << '\n';
+
+    for(unsigned venta, mes, vendedor; std::cin >> venta >> mes >> vendedor;)
+        total[vendedor][mes] += venta;
+        
+    std::cout << "TOTAL DE VENTAS" << '\n';
+
+    for(int i{0}; i<3; ++i)
+    {   std::cout << '\n' << "VENDEDOR " << i << '\n';    
+            for(int j{0};j<12;++j)
+                std::cout <<  "MES " << j << " = " << total[i][j]<< '\n';      
+    };
+}
